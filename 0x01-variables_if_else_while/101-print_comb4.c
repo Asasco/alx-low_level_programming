ebaca6757e4234480853eb26f9432b5a650b0f03
Asasco <asamoahp08@gmail.com>
2022-09-10 15:47:39 -0700

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 012 to 789.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, e, g;
+
+	i = 48;
+	e = 48;
+	g = 48;
+
+	e = 48;
+	while (e < 58)
+	{
+		i = 48;
+			while (i < 58)
+			{
+				g = 58;
+				while (g < 58)
+				{
+					putchar(e);
+					putchar(i);
+					putchar(g);
+					if (i == 56 && e == 55 && g == 57)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+				g++;
+			}
+			i++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
+}
