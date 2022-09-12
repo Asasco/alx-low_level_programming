ff49b69fd7f700296e6bbc85eba89cee1d6f8176
Asasco <asamoahp08@gmail.com>
2022-09-12 10:39:31 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - a simple program to write a line of text
- * this time without the commands frim previous examples
+ * main - a simple program to write a line of text, this time without the
+ *
+ * commands from previous examples
  *
  * Return: 1 when finished
  */
 int main(void)
 {
-	char b[] = and that piece of art is useful" - Dora Korpar, 2015-10-19
+		char b[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	writef(b, sizeof(char), sizeof(b) - 1, stdio);
-	return (1);
+			fwrite(b, sizeof(char), sizeof(b) - 1, stderr);
+			return (1);
 }
