5274736c207599ba706220fb22e991ffb909e15c
Asasco <asamoahp08@gmail.com>
2022-09-13 18:03:13 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,7 +17,35 @@ void print_times_table(int n)
 		for (m = 0; m <= n; m++)
 		{
 			for (a = 0; a <= n; a++)
-+			{
+			{
 				p = m * a;
 				if (c == 0 && p < 10)
 				{
+					printf(", %d", p);
+				}
+				else if (c == 0 && p < 100)
+				{
+					printf(", %d", p);
+				}
+				else if (c == 0 && p < 1000)
+				{
+					printf(", %d", p);
+				}
+				else if (c == 0 && p >= 1000)
+				{
+					printf(",%d", p);
+				}
+				else
+				{
+					printf("%d", p);
+				}
+				c =0;
+			}
+			putchar(10);
+			c = 1;
+		}
+	}
+	else
+	{
+	}
+}
