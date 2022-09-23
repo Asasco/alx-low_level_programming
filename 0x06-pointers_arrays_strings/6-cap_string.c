79603fe0a90ec97b76440f0c2cfc7ac2922f94ef
Asasco <asamoahp08@gmail.com>
2022-09-22 21:47:58 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -36,7 +36,7 @@ char *cap_string(char *str)
 				str[index - 1] == '\n' ||
 				str[index - 1] == ',' ||
 				str[index - 1] == ';' ||
-				str[index - 1] == '-' ||
+				str[index - 1] == '.' ||
 				str[index - 1] == '!' ||
 				str[index - 1] == '?' ||
 				str[index - 1] == '"' ||
@@ -44,7 +44,7 @@ char *cap_string(char *str)
 				str[index - 1] == ')' ||
 				str[index - 1] == '{' ||
 				str[index - 1] == '}')
-		str[index] -= 32;
+			str[index] -= 32;
 	}
 
 	return (str);
