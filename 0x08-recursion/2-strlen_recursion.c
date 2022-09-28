16efbf509daf9102be2cad2724a4be67239be643
Asasco <asamoahp08@gmail.com>
2022-09-28 10:56:37 -0700

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,7 +3,7 @@
   *_strlen_recursion - returns length of a string.
   *@s: pointer to string.
   *
-  *Retrun: length of a string.
+  *Return: length of a string.
   */
 int _strlen_recursion(char *s)
 {
