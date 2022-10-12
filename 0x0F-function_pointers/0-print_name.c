3fc3db54308cf87d295112b740dfaa4b1b403a65
Asasco <asamoahp08@gmail.com>
2022-10-12 14:41:00 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,10 @@
 /**
- * print_name - prints a name.
- * @name: pointer to name.
- * @f: function pointer.
- *
- * Return: void.
- */
+  *print_name - prints a name.
+  *@name: pointer to name.
+  *@f: function pointer.
+  *
+  *Return: void.
+  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
