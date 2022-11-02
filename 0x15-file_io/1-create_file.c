0de29fa8658b152ab4112e70fd44c7f1312d0294
Asasco <asamoahp08@gmail.com>
2022-11-02 07:24:44 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		while (text_content[len])
-			en++;
+			len++;
 	}
 	/* write into file description */
 	count = write(fd, text_content, len);
