5e04e75c6ae55e83d896f2c62de4f3a9934a156a
Asasco <asamoahp08@gmail.com>
2022-11-04 17:16:15 -0700

    task 4

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Libraries */
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -9,17 +8,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-/* MACROS */
-#define BUFFERSIZE 1024
-#define READ_ERROR 1
-#define WRITE_ERROR 2
-
-/* prototypes of functions */
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int _strlen(char *str);
 int append_text_to_file(const char *filename, char *text_content);
-char *create_buffer(char *fileName);
-int error_handler(int fd, char *fname, int type, char *buffer);
-void close_file(int fd);
 
 #endif
